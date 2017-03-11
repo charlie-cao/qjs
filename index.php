f686657a6c71e9db622de31fe8e3e857e9c5e226
qgame1914 <964870916@qq.com>
2017-03-11 18:52:33 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>千家师系统</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+        <link rel="stylesheet" href="./c/public/style/weui.css"/>
+        <link rel="stylesheet" href="./c/public/style/weui2.css"/>
+        <link rel="stylesheet" href="./c/public/style/weui3.css"/>
+        <style>
+        .weui_cell_primary{
+            padding-left:4px;
+        }
+        </style>
+</head>
+<body>
+<div style="text-align: center; padding:30px;">
+    <i class="weui_icon_msg weui_icon_waiting"></i>
+    <h3>千家师系统</h3>
+</div>
+<h4 style="text-align: center;">测试入口</h4>
+<h4 style="text-align: center;">默认学校id：0 默认班级id：0</h4>
+
+<div class="weui_cells weui_cells_access">
+            <a class="weui_cell" href="./c/school/index.php?state=0&test=pt">
+                <div class="weui_cell_hd"><span class="icon icon-29"></span></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>大家庭</p>
+                </div>
+                <div class="weui_cell_ft">授权为普通用户</div>
+            </a>
+            <a class="weui_cell" href="./c/school/index.php?state=0&test=yz">
+                <div class="weui_cell_hd"><span class="icon icon-29"></span></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>大家庭</p>
+                </div>
+                <div class="weui_cell_ft">授权为园长</div>
+            </a>
+            <a class="weui_cell" href="./c/school/index.php?state=0&test=zl">
+                <div class="weui_cell_hd"><span class="icon icon-29"></span></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>大家庭</p>
+                </div>
+                <div class="weui_cell_ft">授权为助理</div>
+            </a>
+</div>
+<div class="weui_cells weui_cells_access">
+            <a class="weui_cell" href="./c/cls/index.php?state=0-0&test=pt">
+                <div class="weui_cell_hd"><span class="icon icon-103"></span></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>慢成长</p>
+                </div>
+                <div class="weui_cell_ft">授权为普通用户</div>
+            </a>
+            <a class="weui_cell" href="./c/cls/index.php?state=0-0&test=yz">
+                <div class="weui_cell_hd"><span class="icon icon-103"></span></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>慢成长</p>
+                </div>
+                <div class="weui_cell_ft">授权为园长</div>
+            </a>
+
+            <a class="weui_cell" href="./c/cls/index.php?state=0-0&test=bzr">
+                <div class="weui_cell_hd"><span class="icon icon-103"></span></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>慢成长</p>
+                </div>
+                <div class="weui_cell_ft">授权为班主任</div>
+            </a>
+</div>
+<div class="weui_cells weui_cells_access">
+
+            <a class="weui_cell" href="./c/qa/index.php?state=0&test=pt">
+                <div class="weui_cell_hd"><span class="icon icon-100"></span></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>千家师</p>
+                </div>
+                <div class="weui_cell_ft">授权为普通用户</div>
+            </a>
+            <a class="weui_cell" href="./c/qa/index.php?state=0&test=zj">
+                <div class="weui_cell_hd"><span class="icon icon-100"></span></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p> 千家师</p>
+                </div>
+                <div class="weui_cell_ft">授权为专家</div>
+            </a>
+        </div>
+</body>
+</html>
