bdeeaa2ce6ae2fd60a4d69d553d7c94dc36dabfc
qgame1914 <964870916@qq.com>
2017-03-11 18:56:28 +0800

    init 更新

---
+++ c/config.php
@@ -0,0 +1,32 @@
+<?php
+
+//debug
+ini_set("display_errors", "On");
+error_reporting(E_ALL);
+ini_set('date.timezone','Asia/Shanghai'); 
+
+//session
+session_start();
+
+//db
+$dbms = 'mysql';     //数据库类型 oracle 用ODI,对于开发者来说，使用不同的数据库，只要改这个，不用记住那么多的函数了
+$host = 'localhost'; //数据库主机名
+$dbName = 'schoolcms';  //使用的数据库
+$user = 'root';      //数据库连接用户名
+$pass = 'MYWy9CaA44uQZW3u';          //对应的密码
+$dsn = "$dbms:host=$host;dbname=$dbName";
+
+
+try {
+    $db = new PDO($dsn, $user, $pass); //初始化一个PDO对象，就是创建了数据库连接对象$dbh
+    $db->query("SET NAMES utf8;");
+} catch (PDOException $e) {
+    die("Error!: " . $e->getMessage() . "<br/>");
+}
+
+
+
+$appid = "wx8dfb25342ca7f279";
+$secret = "d35b3b82fc417eeae0c663fd59eb32a3";
+
+?>
